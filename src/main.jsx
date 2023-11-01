c5692e1034c2f4230144da73d1f13530624506a8
navanee1609 <navaneethanvs18@gmail.com>
2023-11-01 17:03:58 +0530

    installed types bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,18 @@ import {
 } from "react-router-dom";
 import './index.css'
 
+import 'swiper/css';
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// fonts and icons
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
+
 
 const router = createBrowserRouter([
   {
