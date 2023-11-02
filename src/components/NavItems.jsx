6f27d19d09dc2ebd458867faa76f5ff8be352342
navanee1609 <navaneethanvs18@gmail.com>
2023-11-02 23:13:41 +0530

    imported logo

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../assets/images/logo/logo.png";
 
 const NavItems = () => {
      let [menuToggle,setMenuToggle] =useState(false)
@@ -31,8 +32,8 @@ const NavItems = () => {
 
             </div>
         </div>
-         
 
+        
 
 
     </header>
