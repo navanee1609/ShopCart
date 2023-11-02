b29645ec6ea9db2e97a0c332434e257784275476
navanee1609 <navaneethanvs18@gmail.com>
2023-11-02 22:49:12 +0530

    added navitems to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavItems from './components/NavItems'
 
 function App() {
   
 
   return (
    <> 
-   <nav>Navbar</nav>
+   <NavItems/>
   <Outlet/>
    </>
   )
