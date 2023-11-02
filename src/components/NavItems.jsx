e39feb495736248ca5b74f4486f63837c7dc98a7
navanee1609 <navaneethanvs18@gmail.com>
2023-11-02 22:55:51 +0530

    added usestate to the navitems

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NavItems = () => {
+     let [menuToggle,setMenuToggle] =useState(false)
+     let [socialToggle,setSocialToggle]= useState(false)
+     let [headerFixed,setHeaderFixed] = useState(false)
   return (
     <div>NavItems</div>
   )
