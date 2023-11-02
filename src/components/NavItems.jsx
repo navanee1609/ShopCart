f2c19a933a8ba71474f0256b907d8f8545e02d98
navanee1609 <navaneethanvs18@gmail.com>
2023-11-02 22:58:44 +0530

    added logic (egventListener) to headerfixed

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -4,6 +4,16 @@ const NavItems = () => {
      let [menuToggle,setMenuToggle] =useState(false)
      let [socialToggle,setSocialToggle]= useState(false)
      let [headerFixed,setHeaderFixed] = useState(false)
+
+    //  addeventListener (for scrolling)
+
+     window.addEventListener('scroll',()=>{
+        if(window.scrollY > 200){
+            setHeaderFixed(true)
+        }else{
+            setHeaderFixed(false)
+        }
+     })
   return (
     <div>NavItems</div>
   )
