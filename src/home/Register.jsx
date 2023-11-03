2e6157d8393fd9302ee4cb1535b3c09dc7779675
navanee1609 <navaneethanvs18@gmail.com>
2023-11-03 22:43:11 +0530

    added subtitle and title

--- src/home/Register.jsx
+++ src/home/Register.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+const subTitle = "Save The Day";
+const title = (
+  <h2 className="title">
+    Join on Day Long Free Workshop for <b>Advance</b>{" "}
+    <span className="yellow-color">Mastering</span> <b>On Sales</b>
+  </h2>
+);
+
 const Register = () => {
   return (
     <div>Register</div>
