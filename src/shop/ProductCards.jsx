b06cf04ebcbf0aa77ae9987e3a1cc1611b230a8e
navanee1609 <navaneethanvs18@gmail.com>
2023-11-15 11:40:37 +0530

    added castegory section

--- src/shop/ProductCards.jsx
+++ src/shop/ProductCards.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
-import Rating from "../../components/Sidebar/rating";
+// import Rating from "../../components/Sidebar/rating";
 
 const ProductCards = ({ products, GridList }) => {
   // console.log(products.map(val => console.log(val.id)))
@@ -34,9 +34,9 @@ const ProductCards = ({ products, GridList }) => {
               <h5>
                 <Link to={`/shop/${product.id}`}>{product.name}</Link>
               </h5>
-              <p className="productRating">
+              {/* <p className="productRating">
                 <Rating />
-              </p>
+              </p> */}
               <h6>${product.price}</h6>
             </div>
           </div>
@@ -59,9 +59,9 @@ const ProductCards = ({ products, GridList }) => {
             </div>
             <div className="product-content">
             <Link to={`/shop/${product.id}`}>{product.name}</Link>
-              <p className="productRating">
+              {/* <p className="productRating">
                 <Rating />
-              </p>
+              </p> */}
               <h6>${product.price}</h6>
               <p>{product.seller}</p>
             </div>
