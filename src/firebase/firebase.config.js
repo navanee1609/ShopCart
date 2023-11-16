2b0fa9ce15d36b12aa28782436f36abea157bda7
navanee1609 <navaneethanvs18@gmail.com>
2023-11-16 13:13:00 +0530

    configurred firebase with env local variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeWvymAi8---4ymay6sy7TePuOVy6s1ag",
-  authDomain: "shop-cart-react-da100.firebaseapp.com",
-  projectId: "shop-cart-react-da100",
-  storageBucket: "shop-cart-react-da100.appspot.com",
-  messagingSenderId: "538722060419",
-  appId: "1:538722060419:web:63a0b13f04cfe6ef1e73b6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
