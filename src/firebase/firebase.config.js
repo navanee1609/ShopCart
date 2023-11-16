6f98b97aa601de54c1a9fe579b9678d9a518dc49
navanee1609 <navaneethanvs18@gmail.com>
2023-11-16 12:54:15 +0530

    configured firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeWvymAi8---4ymay6sy7TePuOVy6s1ag",
+  authDomain: "shop-cart-react-da100.firebaseapp.com",
+  projectId: "shop-cart-react-da100",
+  storageBucket: "shop-cart-react-da100.appspot.com",
+  messagingSenderId: "538722060419",
+  appId: "1:538722060419:web:63a0b13f04cfe6ef1e73b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
