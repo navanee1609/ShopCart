9c8bd89dc087d2e6f218e845194a6ae24de08633
navanee1609 <navaneethanvs18@gmail.com>
2023-11-16 11:18:29 +0530

    added date in most popular post

--- src/blog/SingleBlog.jsx
+++ src/blog/SingleBlog.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useEffect, useState } from "react";
-import Tags from "../Shop/Tags";
+// import Tags from "../Shop/Tags";
 import PageHeader from "../components/PageHeader";
 import { useParams } from "react-router-dom";
 import blogList from "../utilis/blogdata";
@@ -246,7 +246,7 @@ const SingleBlog = () => {
             </div>
             <div className="col-lg-4 col-12">
               <aside>
-                <Tags />
+                {/* <Tags /> */}
                 <MostPopularPost/>
               </aside>
             </div>
--- src/components/Sidebar/MostPopularPost.jsx
+++ src/components/Sidebar/MostPopularPost.jsx
@@ -7,28 +7,28 @@ const postList = [
         imgUrl: '/src/assets/images/blog/10.jpg',
         imgAlt: 'rajibraj91',
         title: 'Poor People Campaign Our Resources',
-        date: 'Jun 05,2022',
+        date: 'Nov 04, 2023',
     },
     {
         id:2,
         imgUrl: '/src/assets/images/blog/11.jpg',
         imgAlt: 'rajibraj91',
         title: 'Poor Peoples Campaign Our Resources',
-        date: 'Jun 05,2022',
+        date: 'Nov 04, 2023',
     },
     {
         id:3,
         imgUrl: '/src/assets/images/blog/12.jpg',
         imgAlt: 'rajibraj91',
         title: 'Poor Peoples Campaign Our Resources',
-        date: 'Jun 05,2022',
+        date: 'Nov 04, 2023',
     },
     {
         id:4,
         imgUrl: '/src/assets/images/blog/09.jpg',
         imgAlt: 'rajibraj91',
         title: 'Poor Peoples Campaign Our Resources',
-        date: 'Jun 05,2022',
+        date: 'Nov 04, 2023',
     },
 ]
 
