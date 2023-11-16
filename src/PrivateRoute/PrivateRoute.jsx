384e0d8a1fa0b8d94039bc2db75cd5ab731fc119
navanee1609 <navaneethanvs18@gmail.com>
2023-11-16 16:27:56 +0530

    provided private route for loading

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider";
 
+
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
