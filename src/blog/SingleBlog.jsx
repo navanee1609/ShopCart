dba834662a2e2a6f4a27293b8e510d5fe36546dd
navanee1609 <navaneethanvs18@gmail.com>
2023-11-17 12:02:33 +0530

    added link to play youtube video

--- src/blog/SingleBlog.jsx
+++ src/blog/SingleBlog.jsx
@@ -137,7 +137,7 @@ const SingleBlog = () => {
                                 </p>
 
                                 <img
-                                  src="/src/assets/images/blog/single/01.jpg"
+                                  src="/images/blog/single/01.jpg"
                                   alt="rajibraj91"
                                 />
 
@@ -160,11 +160,11 @@ const SingleBlog = () => {
 
                                 <div className="video-thumb">
                                   <img
-                                    src="/src/assets/images/blog/single/02.jpg"
+                                    src="/images/blog/single/02.jpg"
                                     alt="video"
                                   />
                                   <a
-                                    href="https://youtu.be/2qWo6W5Wn8Q"
+                                    href="https://youtu.be/H0XwFHclWjw?si=carmxhs_U0YP2mZ3"
                                     className="video-button popup"
                                     target="_blank"
                                   >
