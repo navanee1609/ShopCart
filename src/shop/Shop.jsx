fd28fb8d4ecfb15ad8b4baad8f7919b3c94fc5ff
navanee1609 <navaneethanvs18@gmail.com>
2023-11-20 09:43:37 +0530

    reassingned product.json's path

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -8,8 +8,8 @@ import PopularPost from "./PopularPost";
 import Tags from "./Tags";
 import ProductCards from "./ProductCards";
 const showResult = "Showing 01 - 12 of 139 Results";
-import Data from "/src/products.json"
-
+// import Data from "/products.json"
+import Data from '../products.json'
 const Shop = () => {
   const [GridList, setGridList] = useState(true);
   const [products, setProducts] = useState(Data);
