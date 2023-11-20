098e8d98f7b98ab25be78c2b17f3b155b34c3cbf
navanee1609 <navaneethanvs18@gmail.com>
2023-11-20 10:01:51 +0530

    modified

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -8,7 +8,7 @@ import PopularPost from "./PopularPost";
 import Tags from "./Tags";
 import ProductCards from "./ProductCards";
 const showResult = "Showing 01 - 12 of 139 Results";
-import Data from "/products.json"
+import Data from "/src/products.json"
 
 const Shop = () => {
   const [GridList, setGridList] = useState(true);
