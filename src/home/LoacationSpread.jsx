235d3a1eaa9bb0fc2078d6ce9b316dc8021083ae
navanee1609 <navaneethanvs18@gmail.com>
2023-11-20 10:31:30 +0530

    again redefined url to location spread

--- src/home/LoacationSpread.jsx
+++ src/home/LoacationSpread.jsx
@@ -7,37 +7,37 @@ const desc = "Buy products on your any device with our app & enjoy your time wha
 
 const clientsList = [
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
     {
-        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgUrl: '/images/clients/avater.jpg',
         imgAlt: 'education thumb rajibraj91 rajibraj',
         text: 'Join with Us',
     },
